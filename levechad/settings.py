d47bed462116e2b70eabec24cb7a7441fcd260a0
shaymargolis <shay@inbar.co.il>
2020-03-15 15:24:11 +0200

    Fix settings for server/ url

--- levechad/settings.py
+++ levechad/settings.py
@@ -33,14 +33,13 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'client.apps.ClientConfig',
+    'server.apps.ServerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'client',
-    'server'
 ]
 
 MIDDLEWARE = [
--- levechad/urls.py
+++ levechad/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('server.urls'))
+    path('server/', include('server.urls'))
 ]
