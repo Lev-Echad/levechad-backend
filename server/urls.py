997266f606c8808a608a476f226bf4c63a55b8ea
Topaz Enbar <topaz.enbar.40@gmail.com>
2020-03-15 23:48:33 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	db.sqlite3
    #	server/templates/server/help_table.html

--- server/urls.py
+++ server/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('volunteer_table', views.show_all_volunteers, name='show_all_volunteers'),
     path('help_table', views.show_all_help_request, name='show_all_help_request'),
-    path('edit/<int:pk>', )
 ]
