14339ce83ca36af46533cc11b9b6c4eb736c2c38
Topaz Enbar <topaz.enbar.40@gmail.com>
2020-03-15 16:34:48 +0200

    before marging topaz

--- server/views.py
+++ server/views.py
@@ -5,5 +5,5 @@ from client.models import Volunteer
 
 def show_all_volunteers(request):
     all_volunteer_data = Volunteer.objects.all()
-    context = {'all_volunteer_data': all_volunteer_data}
+    context = {'volunteer_data': all_volunteer_data}
     return render(request, 'server/volunteer_table.html', context)
