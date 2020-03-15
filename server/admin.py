834cd7343b6fc843aa34c5832c886077df8eea3f
Topaz Enbar <topaz.enbar.40@gmail.com>
2020-03-15 15:49:25 +0200

    first ciycle

--- server/admin.py
+++ server/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 from client.models import Volunteer
+from client.models import Language
+from client.models import City
+from client.models import VolunteerSchedule
+
 
 admin.site.register(Volunteer)
+admin.site.register(Language)
+admin.site.register(City)
+admin.site.register(VolunteerSchedule)
