3e4a4d9d6b8fc6589a046661b89ab2566a15c193
shaymargolis <shay@inbar.co.il>
2020-03-15 15:18:38 +0200

    Add clientConfig to settings

--- levechad/settings.py
+++ levechad/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'client.apps.ClientConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
