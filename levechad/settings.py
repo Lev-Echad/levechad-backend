cadfbd2594bc4340eae58255b3e08c24dc9cf0a4
shaymargolis <shay@inbar.co.il>
2020-03-16 15:46:04 +0200

    Remove heroku from config

--- levechad/settings.py
+++ levechad/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,5 +123,3 @@ USE_TZ = True
 STATIC_ROOT = "" #os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = ( os.path.join('static'), )
 STATIC_URL = "/static/"
-
-django_heroku.settings(locals())
