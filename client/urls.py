c280808848d0d6009555195427f1fe187667a2dc
Royz2123 <1Qazwsxdc>
2020-03-17 02:31:24 +0200

    Added images and whole new style

--- client/urls.py
+++ client/urls.py
@@ -3,13 +3,9 @@ from . import views
 
 
 urlpatterns = [
-#     path('buy_in', views.buy_in, name='show_all'),
-#     path('medic', views.medic, name='show_all'),
-#     path('schedule', views.schedule, name='show_all'),
     path('', views.homepage, name='show_all'),
     path('home', views.homepage, name='show_all'),
     path('thanks', views.thanks, name='show_all'),
-    path('donate', views.thanks, name='show_all'),
 
     path('send_help', views.send_help, name='show_all'),
     path('schedule', views.schedule, name='show_all'),
