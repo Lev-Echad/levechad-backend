3786bc64ccb6a6f719998bfca24b1c3f71155406
shaymargolis <shay@inbar.co.il>
2020-03-17 04:19:46 +0200

    Fix views

--- client/views.py
+++ client/views.py
@@ -120,7 +120,7 @@ def medic_help(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:y
-            return MedicForm('/client/thanks')
+            return HttpResponseRedirect('/client/thanks')
 
     # if a GET (or any other method) we'll create a blank form
     else:
