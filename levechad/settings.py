0569957ee668a7a70a50317b3a99d3c4d3cb3f66
shaymargolis <shay@inbar.co.il>
2020-03-18 15:56:32 +0200

    Remove static dir

--- levechad/settings.py
+++ levechad/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = "" #os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 LOGIN_REDIRECT_URL = '/server'
