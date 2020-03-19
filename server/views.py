6302acfd147c1faa217a4911419cb52df49bb4dd
tehila17-meet <tehilapelled16@gmail.com>
2020-03-19 20:26:18 +0200

    Update views.py

--- server/views.py
+++ server/views.py
@@ -44,7 +44,7 @@ also filters by filter
 """
 @login_required
 def show_all_volunteers(request, page = 1):
-    qs = Volunteer.objects.all().order_by('id')
+    qs = Volunteer.objects.all().order_by('id').reverse()
 
     # ------- filters -------
     areas = request.GET.getlist('area')
@@ -132,7 +132,7 @@ def show_all_volunteers(request, page = 1):
 
     # if there were no matches display all and there are people available
     if len(match_qs) == 0 and (not something_mark):
-        match_qs = Volunteer.objects.all().order_by('id')
+        match_qs = Volunteer.objects.all().order_by('id').reverse()
 
 #     if len(guidings1) != 0:
 #         match_qs = match_qs.filter(guiding=True)
@@ -172,7 +172,7 @@ also filters by filter
 """
 @login_required
 def show_all_help_request(request, page = 1):
-    qs = HelpRequest.objects.all().order_by('id')
+    qs = HelpRequest.objects.all().order_by('id').reverse()
 
     statuses = request.GET.getlist('status')
     type = request.GET.getlist('type')
@@ -207,7 +207,7 @@ def show_all_help_request(request, page = 1):
 
     # if there were no matches display all
     if len(match_qs) == 0 and (not something_mark):
-        match_qs = qs.order_by('id')
+        match_qs = qs.order_by('id').reverse()
 
 
     # ----- orders -----
