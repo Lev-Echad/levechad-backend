5b1600de70aa01e43d7906e4cfbb537f900d249c
yoavmagid <yoavmagid2@gmail.com>
2020-03-30 18:06:56 +0300

    bugfix-68: Fixing volunteer filtering

--- server/views.py
+++ server/views.py
@@ -72,6 +72,9 @@ also filters by filter
 @login_required
 def show_all_volunteers(request, page=1):
     qs = Volunteer.objects.all()
+    filter_options = dict()
+    q_option = Q()
+    availability_now_id = []
 
     # ------- filters -------
     areas = request.GET.getlist('area')
@@ -79,83 +82,61 @@ def show_all_volunteers(request, page=1):
     availability = request.GET.getlist('availability')
     guidings = request.GET.getlist('guiding')
     search_name = request.GET.getlist('searchname')
-    something_mark = False
 
-    area_qs = Volunteer.objects.all().none()
-    language_qs = Volunteer.objects.all().none()
-    availability_qs = Volunteer.objects.all().all()
-
-    area_qs = qs
     if len(get_mandatory_areas(request)) != 0:
-        area_qs = qs.filter(areas__name__in=get_mandatory_areas(request))
+        filter_options['areas__name__in'] = get_mandatory_areas(request)
 
     if len(areas) != 0 and '' not in areas:
-        something_mark = True
-        area_qs = area_qs.filter(areas__name__in=areas)
+        filter_options['areas__name__in'] = filter_options.get('areas__name__in', list()) + areas
 
     if len(lans) != 0 and '' not in lans:
-        something_mark = True
-        language_qs = qs.filter(languages__name__in=lans)
+        filter_options['languages__name__in'] = lans
 
     if len(guidings) != 0:
-        something_mark = True
-        qs = qs.filter(guiding=True)
+        filter_options['guiding'] = True
 
-    if len(search_name) != 0:
-        something_mark = True
+    if len(search_name) != 0 and search_name[0] != '':
         split_name = search_name[0].split()
-        qs = qs.filter(Q(last_name=' '.join(split_name[1:])) | Q(first_name=split_name[0]))
+        q_option |= Q(last_name=' '.join(split_name[1:])) | Q(first_name=split_name[0])
 
     # --------- check time now --------
-    now = datetime.datetime.now()
-    now_day = now.strftime("%A")
+    availability_qs = []
+    if len(availability) != 0:
+        now = datetime.datetime.now()
+        now_day = now.strftime("%A")
 
-    yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
-    yesterday_day = yesterday.strftime("%A")
+        yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
+        yesterday_day = yesterday.strftime("%A")
 
-    # check option 1
-    if is_time_between(time(7, 00), time(15, 00)):
-        schedule_filter = "schedule__" + now_day + "__contains"
-        availability_qs = qs.filter(**{schedule_filter: 1})
+        # check option 1
+        if is_time_between(time(7, 00), time(15, 00)):
+            schedule_filter = "schedule__" + now_day + "__contains"
+            availability_qs = qs.filter(**{schedule_filter: 1})
 
-    # check option 2
-    elif is_time_between(time(15, 00), time(23, 00)):
-        schedule_filter = "schedule__" + now_day + "__contains"
-        availability_qs = qs.filter(**{schedule_filter: 2})
+        # check option 2
+        elif is_time_between(time(15, 00), time(23, 00)):
+            schedule_filter = "schedule__" + now_day + "__contains"
+            availability_qs = qs.filter(**{schedule_filter: 2})
 
+        # check option 3 before midnight
+        elif is_time_between(time(23, 00), time(00, 00)):
+            schedule_filter = "schedule__" + now_day + "__contains"
+            availability_qs = qs.filter(**{schedule_filter: 3})
 
-    # check option 3 before midnight
-    elif is_time_between(time(23, 00), time(00, 00)):
-        schedule_filter = "schedule__" + now_day + "__contains"
-        availability_qs = qs.filter(**{schedule_filter: 3})
+        # check option 3 after midnight
+        elif is_time_between(time(00, 00), time(7, 00)):
+            schedule_filter = "schedule__" + yesterday_day + "__contains"
+            availability_qs = qs.filter(**{schedule_filter: 3})
 
-    # check option 3 after midnight
-    elif is_time_between(time(00, 00), time(7, 00)):
-        schedule_filter = "schedule__" + yesterday_day + "__contains"
-        availability_qs = qs.filter(**{schedule_filter: 3})
+        if availability_qs:
+            for volunteer in availability_qs:
+                availability_now_id.append(volunteer.id)
 
-    availability_now_id = []
-    if availability_qs != []:
-        for volunteer in availability_qs:
-            availability_now_id.append(volunteer.id)
-
-    if len(availability) == 0:
-        availability_qs = Volunteer.objects.all().all()
-
-    # union matchings from both categoties
-    match_qs = area_qs.union(language_qs)
-
-    # if there were no matches display all and there are people available
-    if len(match_qs) == 0 and (not something_mark):
-        match_qs = Volunteer.objects.all()
-
-    availability_qs = (availability_qs)
-    match_qs = match_qs.intersection(availability_qs)
+    match_qs = Volunteer.objects.filter(q_option, **filter_options)
 
     # ----- orders -----
     if 'field' in request.GET:
         field = request.GET.get('field')
-        field = "-" + field
         match_qs = match_qs.order_by(field)
 
     # ----- check for each volunteer how much times he apper
