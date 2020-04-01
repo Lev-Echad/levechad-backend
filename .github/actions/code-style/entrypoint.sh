b4e239b7c639b7d16638832bf40af9abba4bd82d
Guy Omer <a@a.com>
2020-04-01 14:25:25 +0300

    Wrap pycodestyle output in code-block

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -4,6 +4,8 @@
 readonly LINTER_ARGS=$1
 readonly COMMENT_MESSAGE=$2
 
+readonly MARKDOWN_CODE_WRAPPER='```'
+
 if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "The GITHUB_TOKEN is required."
 	echo "Submitting a comment will fail!"
@@ -15,19 +17,15 @@ pycode_output=$(pycodestyle ${LINTER_ARGS} .)
 pycode_retval=$?
 echo $pycode_output
 
-if [[ -z "$COMMENT_MESSAGE" ]]; then
-    echo "No comment message"
-else
-    echo "There is a comment message"
-	commit_message="${COMMENT_MESSAGE}\n${pycode_output}"
-fi
+comment="${COMMENT_MESSAGE}\n${MARKDOWN_CODE_WRAPPER}\n${pycode_output}\n${MARKDOWN_CODE_WRAPPER}"
 
 # If there were errors as part of linting, post a comment. Else, do nothing.
 if [ $pycode_retval -ne 0 ]; then
-  payload=$(echo '{}' | jq --arg body "$pycode_output" '.body = $body')
+  payload=$(echo '{}' | jq --arg body "$comment" '.body = $body')
   comments_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
   curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$payload" "$comments_url"
 else
   echo "There were no pycodestyle issues"
 fi
+
 exit $pycode_retval
