918b465ff11e013dbf8984fdcc8188db2cbfe907
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-12 19:31:08 +0300

    Escape newlines for json

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -15,9 +15,11 @@ cd $GITHUB_WORKSPACE
 
 pycode_output=$(python -m pycodestyle ${LINTER_ARGS} .)
 pycode_retval=$?
+#pycode_output="asd\n"
+#pycode_retval=1
 echo $pycode_output
 
-comment="${COMMENT_MESSAGE}\n${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_CODE_WRAPPER}"
+comment=$(printf "%q" "${COMMENT_MESSAGE}\n${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_CODE_WRAPPER}")
 
 # If there were errors as part of linting, post a comment. Else, do nothing.
 if [ $pycode_retval -ne 0 ]; then
