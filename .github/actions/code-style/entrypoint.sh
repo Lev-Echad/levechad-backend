88f90ecbc8ddb18d765f2a9d48a26572b4807f82
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-12 19:00:24 +0300

    Supply curl input via stdin

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -6,14 +6,14 @@ readonly COMMENT_MESSAGE=$2
 
 readonly MARKDOWN_CODE_WRAPPER='```'
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
+if [ -z "$GITHUB_TOKEN" ]; then
     echo "The GITHUB_TOKEN is required."
     echo "Submitting a comment will fail!"
 fi
 
 cd $GITHUB_WORKSPACE
 
-pycode_output=$(pycodestyle ${LINTER_ARGS} .)
+pycode_output=$(python3 -m pycodestyle ${LINTER_ARGS} .)
 pycode_retval=$?
 echo $pycode_output
 
@@ -23,8 +23,7 @@ comment="${COMMENT_MESSAGE}\n${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_
 if [ $pycode_retval -ne 0 ]; then
   payload=$(echo '{}' | jq --arg body "$comment" '.body = $body')
   comments_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
-  curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data-binary @<(echo "$payload") "$comm
-ents_url"
+  echo -e "$payload\n" | curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data-binary @- "$comments_url"
 else
   echo "There were no pycodestyle issues"
 fi
