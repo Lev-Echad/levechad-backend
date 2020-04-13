19401a4b1176dd59d0b297b04a2b1ffca1417d3c
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-13 18:38:48 +0300

    Use tabs to represent code sections

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -7,12 +7,14 @@ readonly COMMENT_MESSAGE=$2
 cd $GITHUB_WORKSPACE
 
 # Replace newlines with line break
-# sed explained:
+# sed 1 explained:
 #    'a:' label named "a"
 #    'N' Append the next line to the pattern (<br />)
 #    '$!' if not the last line 'ba' branch (goto) label a
 #    's' substitue
-pycode_output=$(python -m pycodestyle ${LINTER_ARGS} . | sed ':a;N;$!ba;s#\n#<br />#g')
+# sed 2 explained:
+#    Adds tab before each line (tab == code line in markdown)
+pycode_output=$(python -m pycodestyle ${LINTER_ARGS} . | sed ':a;N;$!ba;s#\n#<br />#g' | sed 's#^#\t##g')
 
 comment="{\"body\": \"${COMMENT_MESSAGE}<br />${pycode_output}\"}"
 echo -En ${comment} > payload.json
