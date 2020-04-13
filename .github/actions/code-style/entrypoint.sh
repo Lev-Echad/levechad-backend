5412c064becc453f1f198a5875a55c764e1f3c98
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-13 18:47:43 +0300

    Use single backtick instead of tabs

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -9,12 +9,17 @@ cd $GITHUB_WORKSPACE
 # Replace newlines with line break
 # sed 1 explained:
 #    'a:' label named "a"
-#    'N' Append the next line to the pattern (<br />)
+#    'N' Append the next line to the pattern ( pattern = `<br />)
 #    '$!' if not the last line 'ba' branch (goto) label a
 #    's' substitue
 # sed 2 explained:
-#    Adds tab before each line (tab == code line in markdown)
-pycode_output=$(python -m pycodestyle ${LINTER_ARGS} . | sed ':a;N;$!ba;s#\n#<br />#g' | sed 's#^#\t##g')
+#    Complete the backtick started by sed 1
+pycode_output=$(python -m pycodestyle ${LINTER_ARGS} . | sed ':a;N;$!ba;s#\n#`<br />#g' | sed 's#^#`#g')
+
+# If output is empty then there are no linting errors
+if [ -z ${pycode_output} ]; then
+	exit 0
+fi
 
 comment="{\"body\": \"${COMMENT_MESSAGE}<br />${pycode_output}\"}"
 echo -En ${comment} > payload.json
@@ -28,5 +33,5 @@ comments_endpoint=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
 
 curl -sS -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: Application/json" --data-binary @payload.json ${comments_endpoint}
 
-exit 0
-
+# Always fail, if we reach here ther are e linting errors
+exit 1
