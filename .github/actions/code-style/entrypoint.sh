9ca755ebdcd59d6dcaa7a3d9643c61c6d3566a3b
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-13 16:15:25 +0300

    Use external PR comments action

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -6,30 +6,15 @@ readonly COMMENT_MESSAGE=$2
 
 readonly MARKDOWN_CODE_WRAPPER='```'
 
-if [ -z "$GITHUB_TOKEN" ]; then
-    echo "The GITHUB_TOKEN is required."
-    echo "Submitting a comment will fail!"
-fi
-
 cd $GITHUB_WORKSPACE
 
 pycode_output=$(python -m pycodestyle ${LINTER_ARGS} .)
 pycode_retval=$?
-#pycode_output='asd\n'
-#pycode_retval=1
 echo $pycode_output
 
-comment=$(echo -E "${COMMENT_MESSAGE}\\n${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_CODE_WRAPPER}" | sed 's/\\n/\\\\n/g')
+comment="${COMMENT_MESSAGE}\\n${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_CODE_WRAPPER}"
 
-# If there were errors as part of linting, post a comment. Else, do nothing.
-if [ $pycode_retval -ne 0 ]; then
-  payload=$(jq -Rn --arg body "$comment" '.body = $body')
-  echo $payload
-  comments_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
-  echo -E "$payload\\n" | curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data-binary @- "$comments_url"
-else
-  echo "There were no pycodestyle issues"
-fi
+echo "::set-output name=comment::${comment}"
 
 exit $pycode_retval
 
