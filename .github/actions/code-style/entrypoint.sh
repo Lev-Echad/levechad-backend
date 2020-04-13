33cd281bd08fc8d5d18b09811f60a4cbcd8d74fa
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-13 16:19:28 +0300

    Fix exit code for code-style action

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -9,12 +9,10 @@ readonly MARKDOWN_CODE_WRAPPER='```'
 cd $GITHUB_WORKSPACE
 
 pycode_output=$(python -m pycodestyle ${LINTER_ARGS} .)
-pycode_retval=$?
-echo $pycode_output
 
 comment="${COMMENT_MESSAGE}\\n${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_CODE_WRAPPER}"
 
-echo "::set-output name=comment::${comment}"
+echo -E "::set-output name=comment::${comment}"
 
-exit $pycode_retval
+exit 0
 
