a0368033c5f2c165b2758ca70776a0620498241f
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-13 17:17:22 +0300

    Rollback to self Commenting

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -11,10 +11,11 @@ cd $GITHUB_WORKSPACE
 pycode_output=$(python -m pycodestyle ${LINTER_ARGS} .)
 
 comment="${COMMENT_MESSAGE}\\n${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_CODE_WRAPPER}"
-echo "This is comment: ${comment}"
+echo -E $comment | tee comment.json 
 
-#echo "::set-output name=comment::${comment}"
-echo "::set-output name=COMMENT::inline-test"
+comments_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+
+curl -sS -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: Application/json" --data-binary @comment.json $comments_url
 
 exit 0
 
