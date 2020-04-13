65a96cb79e38a522c00ebde6395c423eef9cbe62
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-13 18:25:40 +0300

    Escape newline as soon as possible, so bash won't drop them

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -5,24 +5,27 @@ readonly LINTER_ARGS=$1
 readonly COMMENT_MESSAGE=$2
 
 readonly MARKDOWN_CODE_WRAPPER='```'
-readonly MARKDOWN_LINEBREAK='<br />'
-
 cd $GITHUB_WORKSPACE
 
-pycode_output=$(python -m pycodestyle ${LINTER_ARGS} . | sed 's#\\#${MARKDOWN_LINEBREAK}#g')
+# Replace newlines with line break
+# sed explained:
+#    'a:' label named "a"
+#    'N' Append the next line to the pattern (<br />)
+#    '$!' if not the last line 'ba' branch (goto) label a
+#    's' substitue
+pycode_output=$(python -m pycodestyle ${LINTER_ARGS} . | sed ':a;N;$!ba;s/\n/<br \/>/g')
 
-comment="{\"body\": \"${COMMENT_MESSAGE}${MARKDOWN_LINEBREAK}${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_CODE_WRAPPER}\"}"
+comment="{\"body\": \"${COMMENT_MESSAGE}<br />${MARKDOWN_CODE_WRAPPER}${pycode_output}${MARKDOWN_CODE_WRAPPER}\"}"
 echo -En ${comment} > payload.json
 cat payload.json
 
-# Escape backspaces
+# Escape backslashes if any
 sed -i 's#\\#\\\\#g' payload.json
 
-comments_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
-echo ${comments_url}
-cat ${GITHUB_EVENT_PATH}
+# Endpoint for posting comments
+comments_endpoint=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
 
-curl -sS -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: Application/json" --data-binary @payload.json ${comments_url}
+curl -sS -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: Application/json" --data-binary @payload.json ${comments_endpoint}
 
 exit 0
 
