27a26a0d4aeea4fe28c0aa32812a851617a123a6
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-13 17:53:32 +0300

    Debug comments url

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -18,8 +18,10 @@ cat payload.json
 sed -i "s#\\#\\\\#g" payload.json
 
 comments_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+cat ${comments_url}
+cat ${GITHUN_EVENT_PATH}
 
-curl -sS -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: Application/json" --data-binary @comment.json $comments_url
+curl -sS -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: Application/json" --data-binary @comment.json ${comments_url}
 
 exit 0
 
