a54e82955587a3aec62ccc5297573a6668c6817a
Yuval Krinsky <33120303+ykrinsky@users.noreply.github.com>
2020-04-14 16:16:36 +0300

    Bugfix/117 fix phone number validation (#264)
    * Moved phone number validator into clients/validators and added use of it in api/views and in VolunteerForm.
    
    * Fixed bug #49 -  added 07 prefix.
    
    * Fixed CR: added hypen support and reorganized the phone number regex.
    
    * Fixed unrelated minor issues raised by the new linter.
    
    * Fixed CR: added support to more hypens placing.

--- client/validators.py
+++ client/validators.py
@@ -1,7 +1,16 @@
 import re
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 ID_NUMBER_REGEX = r'^\d{8,9}$'
+
+
+# Checks if the phone number starts with +972/0, and then if the second part is one of:
+# 1. a land line geographic based number - 2/3/4/8/9 and afterwards 7 digits.
+# 2. a new number country prefix - 7 and afterwards 8 digits.
+# 3. a mobile number - 5 and afterwards 8 digits.
+PHONE_NUMBER_REGEX = re.compile(r"^(\+972[- ]?|0)([23489]|[57]\d)[- ]?((\d{3}-?\d{4})|(\d{4}-?\d{3}))$")
+phone_number_validator = RegexValidator(PHONE_NUMBER_REGEX)
 
 
 def id_number_validator(value):
