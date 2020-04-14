fc70af62516679b8dfd7d8524cce901b238fe8b7
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-14 13:26:36 +0300

    Post error comment if something goes wrong

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -6,7 +6,8 @@ readonly COMMENT_MESSAGE=$2
 
 cd $GITHUB_WORKSPACE
 
-pycode_output="$(python -m pycodestyle ${LINTER_ARGS} . | sed 's#^#*#g')"
+# Execute pycodestyle, escape the literal '\.' (might occur in W605), prepend each line with '`*', and append '`'
+pycode_output="$(python -m pycodestyle ${LINTER_ARGS} . | sed 's#\\.#\\\\.#g' | sed 's#^#* \`#g' | sed 's#$#\`#g')"
 
 # If output is empty then there are no linting errors
 if [ -z ${pycode_output} ]; then
@@ -15,19 +16,30 @@ fi
 
 comment="{\"body\": \"${COMMENT_MESSAGE}\r\n\n${pycode_output}\"}"
 echo -En ${comment} > payload.json
-cat payload.json
+#cat payload.json
 
 # Escape backslashes if any
-sed -i 's#\\#\\\\#g' payload.json
+#sed -i 's#\\#\\\\#g' payload.json
 
 # Endpoint for posting comments
 comments_endpoint=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
 
-curl -sS \
+response_code="$(curl -sS \
 	-H "Authorization: token ${GITHUB_TOKEN}" \
 	-H "Content-Type: Application/json" \
 	--data-binary @payload.json \
-	${comments_endpoint}
+	-w "%{http_code}\n" \
+	${comments_endpoint} | tail -1)"
+
+# If something went wrong, try notfiying with a comment
+if [  ${response_code} -eq 200 ]; then
+	curl -sS \
+		-H "Authorization: token ${GITHUB_TOKEN}" \
+		-H "Content-Type: Application/json" \
+		--data '{"body": "Something went wrong! Please check this workflow!"}'
+		${comments_endpoint}
+fi
+	
 
 # Always fail, if we reach here there are linting errors
 exit 1
