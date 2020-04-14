eab03413d141f351905c2ec9ad4222767109c844
Guy Omer <6340924+GuyOmer@users.noreply.github.com>
2020-04-14 15:04:04 +0300

    Cleanup debug and docs

--- .github/actions/code-style/entrypoint.sh
+++ .github/actions/code-style/entrypoint.sh
@@ -8,14 +8,10 @@ cd $GITHUB_WORKSPACE
 
 # 1. Execute pycodestyle
 # 2. Escape the literal '\.' (it's invalid in json)
-# 3. Prepend each line with 
+# 3. Prepend each line with '* \`', append it with '`' and replace newline (at end of line) with an escaped newline
 pycode_output="$(python -m pycodestyle ${LINTER_ARGS} . | \
 	sed 's#\\.#\\\\.#g' | \
 	awk '{print "* \`", $0, "\`"; }' ORS='\\n')"
-	#sed 's#^#* \`#g' | \
-	#sed 's#$#\`#g' | \
-	#awk 1 ORS='\n')"
-	##sed ':a;N;$!ba;s#\n#\\n#g')"
 
 # If output is empty then there are no linting errors
 if [ -z "${pycode_output}" ]; then
@@ -24,24 +20,19 @@ fi
 
 comment="{\"body\": \"${COMMENT_MESSAGE}\r\n\n${pycode_output}\"}"
 echo -En "${comment}" > payload.json
-cat payload.json
-
-# Escape backslashes if any
-#sed -i 's#\\#\\\\#g' payload.json
 
 # Endpoint for posting comments
 comments_endpoint=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
 
-set -x
 curl_ouput="$(curl -sS \
 	-H 'Authorization: token '${GITHUB_TOKEN} \
 	-H 'Content-Type: Application/json' \
 	--data-binary @payload.json \
 	-w '%{http_code}\n' \
 	${comments_endpoint})"
-	response_code="$(echo "${curl_ouput}" | tail -1)"
+response_code="$(echo "${curl_ouput}" | tail -1)"
 
-# If something went wrong, try notfiying with a comment (201 - Created is expected)
+# 201 - "Created" is expected
 if [  ${response_code} -ne 201 ]; then
 	echo "Something went wrong, post a warning comment!"
 	curl -sS \
