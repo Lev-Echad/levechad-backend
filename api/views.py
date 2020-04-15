3ca771abf0fdcae8e5b38f14ccac7bb2119526f3
mikikam2 <62796942+mikikam2@users.noreply.github.com>
2020-04-15 16:56:56 +0300

    added an option to filter with parameters in the volunteers endpoint. (#234)
    * added an option to filter with parameters in the volunteers endpoint.
    
    * CR - added option to filter on one of cities, and a filter for organizations (one of or exact). Also cosmetic changes
    
    * Added libs to requirements.txt, and removed unused imports
    
    * Added the time volunteered as annotation as @astar26 suggested, and changed the filter class to fit
    
    * changed access type
    
    * added filter type to settings.py
    
    * added django filters to installed apps
    
    * Updated API documentation and requirements
    
    Co-authored-by: Stav Locker <stavlocker@gmail.com>

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 import re
 
+from django.db.models import Count
 from rest_framework import viewsets, generics, mixins
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+import django_filters as filters
 from client.models import Volunteer
 from client.validators import PHONE_NUMBER_REGEX
 from api.serializers import VolunteerSerializer, RegistrationSerializer
@@ -58,7 +60,42 @@ class RegistrationAPIViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = RegistrationSerializer
 
 
+class VolunteerFilter(filters.FilterSet):
+    time_volunteered = filters.NumberFilter(lookup_expr='exact')
+    time_volunteered__gt = filters.NumberFilter(method='time_volunteered_gt')
+    time_volunteered__lt = filters.NumberFilter(method='time_volunteered_lt')
+
+    def time_volunteered_gt(self, queryset, field_name, value):
+        return queryset.filter(time_volunteered__gt=value)
+
+    def time_volunteered_lt(self, queryset, field_name, value):
+        return queryset.filter(time_volunteered__lt=value)
+
+    class Meta:
+        model = Volunteer
+        fields = {
+            'id': ['exact'],
+            'first_name': ['exact', 'icontains'],
+            'last_name': ['exact', 'icontains'],
+            'tz_number': ['exact', 'icontains'],
+            'phone_number': ['exact', 'icontains'],
+            'date_of_birth': ['gt', 'lt', 'exact'],
+            'age': ['gt', 'lt', 'exact'],
+            'gender': ['exact'],
+            'city': ['exact', 'in'],
+            'neighborhood': ['exact', 'icontains'],
+            'areas': ['exact'],
+            'moving_way': ['exact'],
+            'week_assignments_capacity': ['exact', 'range'],
+            'wanted_assignments': ['exact'],
+            'score': ['exact'],
+            'created_date': ['gt', 'lt', 'exact'],
+            'organization': ['exact', 'in']
+        }
+
+
 class ListVolunteersViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    queryset = Volunteer.objects.all().order_by('-created_date')
+    queryset = Volunteer.objects.all().order_by('-created_date').annotate(time_volunteered=Count('helprequest'))
     serializer_class = VolunteerSerializer
     permission_classes = [IsAuthenticated]
+    filterset_class = VolunteerFilter
