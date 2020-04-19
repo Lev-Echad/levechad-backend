a3f6b672257c0a973fe37c05cd155a0f0c11c3cc
moshezvili <moshezvili@gmail.com>
2020-04-20 00:37:51 +0300

    remove unnecessary dependencies

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework import routers, permissions
-from rest_framework_swagger.views import get_swagger_view
 import api.views
 
 from drf_yasg.views import get_schema_view
